ccae0faedd9c5ba3261b35b8c18426725e9ae250
jenga-sudo <j53167364@gmail.com>
2025-10-04 00:46:37 +0900

    555
    1111

--- src/main/java/com/yourname/feurshadowelytra/FeurShadowElytra.java
+++ src/main/java/com/yourname/feurshadowelytra/FeurShadowElytra.java
@@ -5,8 +5,10 @@ import net.minecraftforge.fml.common.Mod;
 @Mod(FeurShadowElytra.MODID)
 public class FeurShadowElytra {
     public static final String MODID = "feur_shadow_elytra";
+
     public FeurShadowElytra() {
         ModItems.register();
         ClientInit.register();
+        ModCreative.register();
     }
 }
