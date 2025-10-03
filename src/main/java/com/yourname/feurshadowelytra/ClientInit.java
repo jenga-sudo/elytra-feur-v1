757b8fe4febb8fedc2d9cd6b560480029cf13538
jenga-sudo <j53167364@gmail.com>
2025-10-03 23:39:03 +0900

    Update ClientInit.java

--- src/main/java/com/yourname/feurshadowelytra/ClientInit.java
+++ src/main/java/com/yourname/feurshadowelytra/ClientInit.java
@@ -6,18 +6,18 @@ import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.DistExecutor;
 
+import net.minecraft.client.renderer.entity.player.PlayerRenderer;
+
 public class ClientInit {
     public static void register() {
         DistExecutor.safeRunWhenOn(Dist.CLIENT, () -> ClientOnly::init);
     }
+
     private static class ClientOnly {
         static void init() { MinecraftForge.EVENT_BUS.register(new ClientOnly()); }
+
         @SubscribeEvent
         public void addLayers(EntityRenderersEvent.AddLayers e) {
             e.getSkins().forEach(skin -> {
                 var r = e.getSkin(skin);
-                if (r != null) r.addLayer(new SpriteWingsLayer(r));
-            });
-        }
-    }
-}
+                if (r insta
